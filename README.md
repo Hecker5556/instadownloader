bafbe040430473e6181f7f6b7183464816669c56
unknown <christianmolicki@gmail.com>
2023-12-29 20:27:42 +0100

    support for images with music

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
 # Simple Instagram post and reels downloader
-
+Downloads reels, stories, posts with multiple media, and reels with music (combines them using ffmpeg)
 ## First time setup
-### in command prompt:
-    git clone https://github.com/Hecker5556/instadownloader.git
-#
-    cd instadownloader
-### If you haven't already, download [python.](https://python.org)
+in command prompt:
+```bash
+git clone https://github.com/Hecker5556/instadownloader.git
+``````
+
+```bash
+cd instdownloader 
+```
+If you haven't already, download [python.](https://python.org)
+download [ffmpeg](https://www.ffmpeg.org/download.html)
 ### Optional: build to exe (windows) and add to path, linux users, this wont build to exe but it will add to path, chmod it and write into a file what command to use to execute it
     python setup.py build
 ### if you dont want to do that, just do:
     pip install -r requirements.txt
 
 ## Get sessionid
-### Before you start running the code, you will need to add a sessionid (and csrftoken to avoid getting temporarily disabled) to a .env file, which you can get by either checking network traffic on instagram or using a cookie viewing extension
+Before you start running the code, you will need to add a sessionid (and csrftoken to decrease chances of getting temporarily disabled) to a .env file, which you can get by either checking network traffic on instagram or using a cookie viewing extension
 ## Video tutorial why not 
 https://github.com/Hecker5556/instadownloader/assets/96238375/ecb26c67-24f7-4331-80ab-69ef560a45cc
 
--- insta.py
+++ insta.py
@@ -1,6 +1,6 @@
 import requests, json, re, os
 from tqdm.asyncio import tqdm
-from datetime import datetime
+from datetime import datetime, timedelta
 import argparse
 import env
 import logging
@@ -23,6 +23,7 @@ class instadownloader:
         logging.basicConfig(level=logging.DEBUG, format="%(message)s")
         logging.debug(link)
         allmedia = r'(\{\"require\":\[\[\"ScheduledServerJS\",\"handle\",null,\[\{\"__bbox\":\{\"require\":\[\[\"RelayPrefetchedStreamCache\",\"next\",\[\],\[\"adp_PolarisPostRootQueryRelayPreloader(?:.*?))</script>'
+        musicmedia = r"({\"require\":\[\[\"ScheduledServerJS\",\"handle\",null,\[\{\"__bbox\":{\"require\":\[\[\"PolarisQueryPreloaderCache\",\"add\",\[\],\[\{\"preloaderID\":\"891618966000029196\",\"data\":{\"__bbox\":{\"complete\":true,\"request\":{\"method\":\"GET\",\"url\":\"\\/api\\/v1\\/feed\\/user\\/{user_id}\\/\",\"params\":{\"path\":{\"user_id\":\"29474634220\"},\"query\":{\"count\":7}}},\"result\":{\"response\":\"{\\\"items\\\":(?:.*?))</script>"
         cookies = {
             'sessionid': sessionid,
             'csrftoken': csrftoken
@@ -52,6 +53,9 @@ class instadownloader:
             print('multiple')
             post = 'multiple'
             matches = re.findall(allmedia, rtext, re.MULTILINE)
+            musicmatches = re.findall(musicmedia, rtext, re.MULTILINE)
+            with open("response.json", "w") as f1:
+                json.dump(json.loads(matches[0]), f1, indent=4)
             if matches:
                 def find_key(json_obj, target_key):
                     if isinstance(json_obj, dict):
@@ -71,8 +75,21 @@ class instadownloader:
                 match = find_key(json.loads(matches[0]), 'carousel_media')
                 if not match:
                     print('actually single image')
+                    print("music_metadata" in rtext)
                     post = 'image'
                     match = find_key(json.loads(matches[0]), 'image_versions2')
+                    musicinfo = None
+                    if musicmatches:
+                        response = json.loads(find_key(json.loads(musicmatches[0]), 'response'))
+                        musicmetadata = find_key(response, 'music_metadata')
+                        downloadurl = musicmetadata["music_info"]["music_asset_info"]["fast_start_progressive_download_url"]
+                        durationms = musicmetadata["music_info"]["music_consumption_info"]["overlap_duration_in_ms"]
+                        startms = musicmetadata["music_info"]["music_consumption_info"]["audio_asset_start_time_in_ms"]
+                        endms = startms + durationms
+                        start = f"{int((startms/1000)//3600):02}:{int((startms/1000)//60 % 60):02}:{int((startms/1000) % 60):02}"
+                        end = f"{int((endms/1000)//3600):02}:{int((endms/1000)//60 % 60):02}:{int((endms/1000) % 60):02}"
+                        print(start, end)
+                        musicinfo = {"url": downloadurl, "start": start, "end": end, "duration": durationms/1000}
                 media: dict= {}
                 if post != 'image':
                     try:
@@ -89,6 +106,7 @@ class instadownloader:
                             print(match[char-25:char+25])
                 else:
                     media = {'jpg': match['candidates'][0].get('url')}
+                    media["music"] = musicinfo
 
         elif 'reel' in link:
             post = 'reel'
@@ -161,13 +179,36 @@ class instadownloader:
             print('some error occured')
             return False
         filenames = []
-        tasks = []
+        if post == 'image':
+            files2 = {}
+            musicinfo = None
         for index, (key, value) in enumerate(media.items()):
-            filename = f'{username}-{round(datetime.now().timestamp())}-{index}.{"jpg" if "jpg" in key else "mp4"}'
-            filenames.append(filename)
-            filesizes = {}
-            tasks.append(instadownloader.downloadworker(link = value, filename=filename))
-        await asyncio.gather(*tasks)
+            if key != "music":
+                filename = f'{username}-{round(datetime.now().timestamp())}-{index}.{"jpg" if "jpg" in key else "mp4"}'
+                filenames.append(filename)
+                if post == 'image':
+                    files2["image"] = filename
+                filesizes = {}
+                await instadownloader.downloadworker(link = value, filename=filename)
+            else:
+                 if not value:
+                     continue
+                 filename = f'{username}-{round(datetime.now().timestamp())}.m4a'
+                 await instadownloader.downloadworker(link=value.get('url'), filename=filename)
+                 files2["audio"] = filename
+                 musicinfo = value
+        if post == 'image'and musicinfo:
+            import subprocess
+            filename = f"trimmed-{round(datetime.now().timestamp())}.m4a"
+            command = f"ffmpeg -i {files2['audio']} -ss {musicinfo['start']} -v quiet -to {musicinfo['end']} -c copy {filename}".split()
+            subprocess.run(command)
+            outputfile = files2['image'].replace('jpg', 'mp4')
+            command = f"ffmpeg -loop 1 -i {files2['image']} -i {filename} -v quiet -c:v libx264 -c:a aac -r 2 -tune stillimage -strict experimental -b:a 192k -t {musicinfo['duration']} {outputfile}".split()
+            subprocess.run(command)
+            for key, value in files2.items():
+                os.remove(value)
+            os.remove(filename)
+            filenames = [outputfile]
         for i in filenames:
             filesizes[i] = str(round(os.path.getsize(i)/(1024*1024),2)) + ' mb'
         return filenames, filesizes, post
